9b421d38365ed857489bcb9854b2bdf88603ca65
mominashfak786 <ashfakmomin2001@gmail.com>
2023-08-13 02:29:52 +0530

    It's Working

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-## Getting Started
+<p>Hello, I am Momin Ashfak. I have developed a Weather Forecast  using Java programming language as part of an assignment.</p>
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+<h2>How to Run the Project</h2>
 
-## Folder Structure
+<ol>
+  <li>Clone the project repository from <a href="https://github.com/mominashfak786/Assignment.git">GitHub</a>.</li>
+  <li>Open the project in Visual Studio Code.</li>
+  <li>Run the <code>WeatherApp.java</code> file.</li>
+  <li>The app will establish a connection to the OpenWeatherMap API and fetch hourly weather forecast data for London.</li>
+  <li>The user will be prompted to choose an option:</li>
+</ol>
 
-The workspace contains two folders by default, where:
+<ul>
+  <li>Enter <p>1</p> to get temperature for a specific date and time.</li>
+  <li>Enter <p>2</p> to get wind speed for a specific date and time.</li>
+  <li>Enter <p>3</p> to get pressure for a specific date and time.</li>
+  <li>Enter <p>0</p> to exit the app.</li>
+</ul>
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+<p>Based on the user's input, the app will display the requested weather information.</p>
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+<h1>Thank You</h1>
--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -1,110 +1,166 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.Scanner;
 
 public class WeatherApp {
-
     private static final String BASE_URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22";
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        try {
+            URI endpointUri = new URI(BASE_URL);
+            URL endpoint = endpointUri.toURL();
+            HttpURLConnection conn = (HttpURLConnection) endpoint.openConnection();
+            conn.setRequestMethod("GET");
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+                Scanner sc = new Scanner(conn.getInputStream());
+                String line = "";
+                while (sc.hasNext()) {
+                    line += sc.nextLine();
+                }
+                sc.close();
+
+                WeatherApp weatherApp = new WeatherApp();
+                weatherApp.processWeatherForecast(line);
+
+            } else {
+                System.out.println("Something Went Wrong!");
+            }
+
+        } catch (Exception exc) {
+            System.out.print(exc);
+        }
+    }
+
+    private void processWeatherForecast(String json) {
+        Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("1. Get weather");
-            System.out.println("2. Get Wind Speed");
-            System.out.println("3. Get Pressure");
-            System.out.println("0. Exit");
+            System.out.println("\nChoose an option for getting weather forecast:\n"
+                    + "1. Temperature\n"
+                    + "2. Wind Speed\n"
+                    + "3. Pressure\n"
+                    + "0. Exit");
+
             System.out.print("Enter your choice: ");
-            String choice = reader.readLine();
+            int choice = sc.nextInt();
+            sc.nextLine();
 
             switch (choice) {
-                case "1":
-                    System.out.print("Enter the date (yyyy-MM-dd HH:mm:ss): ");
-                    String targetDate = reader.readLine();
-                    double temperature = fetchData(targetDate, "\"main\":{\"temp\":");
-                    if (temperature != Double.MIN_VALUE) {
-                        System.out.println("Temperature on " + targetDate + ": " + temperature + "°C");
-                    } else {
-                        System.out.println("Data not found for the specified date.");
-                    }
+                case 1:
+                    System.out.print("Enter the date and time (yyyy-MM-dd HH:mm:ss) ");
+                    String targetDateTime = sc.nextLine();
+                    getTemperature(json, targetDateTime);
                     break;
-
-                case "2":
-                    System.out.print("Enter the date (yyyy-MM-dd HH:mm:ss): ");
-                    targetDate = reader.readLine();
-                    double windSpeed = fetchData(targetDate, "\"wind\":{\"speed\":");
-                    if (windSpeed != Double.MIN_VALUE) {
-                        System.out.println("Wind Speed on " + targetDate + ": " + windSpeed + " m/s");
-                    } else {
-                        System.out.println("Data not found for the specified date.");
-                    }
+                case 2:
+                    System.out.print("Enter the date and time (yyyy-MM-dd HH:mm:ss)  ");
+                    targetDateTime = sc.nextLine();
+                    getWindSpeed(json, targetDateTime);
                     break;
-
-                case "3":
-                    System.out.print("Enter the date (yyyy-MM-dd HH:mm:ss): ");
-                    targetDate = reader.readLine();
-                    double pressure = fetchData(targetDate, "\"main\":{\"pressure\":");
-                    if (pressure != Double.MIN_VALUE) {
-                        System.out.println("Pressure on " + targetDate + ": " + pressure + " hPa");
-                    } else {
-                        System.out.println("Data not found for the specified date.");
-                    }
+                case 3:
+                    System.out.print("Enter the date and time (yyyy-MM-dd HH:mm:ss)  ");
+                    targetDateTime = sc.nextLine();
+                    getPressure(json, targetDateTime);
                     break;
-
-                case "0":
-                    System.out.println("Exiting the program.");
-                    return;
-
+                case 0:
+                    System.out.println("Thanks for using");
+                    sc.close();
+                    System.exit(0);
+                    break;
                 default:
-                    System.out.println("Invalid choice. Please enter a valid option.");
+                    System.out.println("Please enter a valid option.");
                     break;
             }
         }
     }
 
-    private static double fetchData(String targetDate, String field) throws IOException {
-        URL url = new URL(BASE_URL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+    private void getTemperature(String json, String targetDateTime) {
+        try {
+            SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            inputDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date inputDate = inputDateFormat.parse(targetDateTime);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        StringBuilder response = new StringBuilder();
+            JSONObject object = new JSONObject(json);
+            JSONArray list = object.getJSONArray("list");
 
-        while ((line = reader.readLine()) != null) {
-            response.append(line);
-        }
-        reader.close();
-        connection.disconnect();
+            for (int i = 0; i < list.length(); i++) {
+                JSONObject inList = list.getJSONObject(i);
+                String forecastDateTime = inList.getString("dt_txt");
+                Date forecastDate = inputDateFormat.parse(forecastDateTime);
 
-        String responseJson = response.toString();
-
-        Pattern pattern = Pattern.compile("\"dt_txt\":\"(.*?)\"");
-        Matcher matcher = pattern.matcher(responseJson);
-
-        while (matcher.find()) {
-            String responseDate = matcher.group(1);
-            LocalDateTime responseDateTime = LocalDateTime.parse(responseDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            LocalDateTime targetDateTime = LocalDateTime.parse(targetDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            
-            if (responseDateTime.isEqual(targetDateTime)) {
-                int startIndex = responseJson.indexOf(field, matcher.end());
-                if (startIndex != -1) {
-                    int endIndex = responseJson.indexOf(",", startIndex);
-                    if (endIndex != -1) {
-                        String valueStr = responseJson.substring(startIndex + field.length(), endIndex).trim();
-                        return Double.parseDouble(valueStr);
-                    }
+                if (inputDate.compareTo(forecastDate) == 0) {
+                    JSONObject main = inList.getJSONObject("main");
+                    double temperature = main.getDouble("temp");
+                    System.out.println("Temperature on " + forecastDateTime + " is " + temperature);
+                    return;
                 }
             }
-        }
 
-        return Double.MIN_VALUE;
+            System.out.println("Data not found");
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+    private void getPressure(String json, String targetDateTime) {
+        try {
+            SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            inputDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date inputDate = inputDateFormat.parse(targetDateTime);
+
+            JSONObject object = new JSONObject(json);
+            JSONArray list = object.getJSONArray("list");
+
+            for (int i = 0; i < list.length(); i++) {
+                JSONObject inList = list.getJSONObject(i);
+                String forecastDateTime = inList.getString("dt_txt");
+                Date forecastDate = inputDateFormat.parse(forecastDateTime);
+
+                if (inputDate.compareTo(forecastDate) == 0) {
+                    JSONObject main = inList.getJSONObject("main");
+                    double pressure = main.getDouble("pressure");
+                    System.out.println("Pressure on " + forecastDateTime + " is " + pressure);
+                    return;
+                }
+            }
+
+            System.out.println("Data not found");
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+    private void getWindSpeed(String json, String targetDateTime) {
+        try {
+            SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            inputDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date inputDate = inputDateFormat.parse(targetDateTime);
+
+            JSONObject object = new JSONObject(json);
+            JSONArray list = object.getJSONArray("list");
+
+            for (int i = 0; i < list.length(); i++) {
+                JSONObject inList = list.getJSONObject(i);
+                String forecastDateTime = inList.getString("dt_txt");
+                Date forecastDate = inputDateFormat.parse(forecastDateTime);
+
+                if (inputDate.compareTo(forecastDate) == 0) {
+                    JSONObject wind = inList.getJSONObject("wind");
+                    double windSpeed = wind.getDouble("speed");
+                    System.out.println("Wind Speed on " + forecastDateTime + " is " + windSpeed);
+                    return;
+                }
+            }
+
+            System.out.println("Data not found ");
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
     }
 }
